847f22818bed0f00969cdf127dd555e50be8fb4e
Enno Gröper <groepeen@cms.hu-berlin.de>
2017-01-10 20:13:46 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-shibboleth-eds',
-    version='0.2',
+    version='0.3',
     packages=find_packages(),
     include_package_data=True,
     license='Apache 2.0',
